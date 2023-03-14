2457b32cc42d9cb6f350395321aab0e54cdb9c1d
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-03-14 05:34:50 +0000

    modified test_file_storage.py code

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,17 +11,44 @@ from models.engine.file_storage import FileStorage
 
 class test_fileStorage(unittest.TestCase):
     """Test FileStorage Class"""
-    def test_instances(self):
-        """chequeamos instantation"""
-        obj = FileStorage()
-        self.assertIsInstance(obj, FileStorage)
+    def setUp(self):
+        self.storage = FileStorage()
 
-    def test_docs(self):
-        """Test docstrings"""
-        self.assertIsNotNone(FileStorage.all)
-        self.assertIsNotNone(FileStorage.new)
-        self.assertIsNotNone(FileStorage.save)
-        self.assertIsNotNone(FileStorage.reload)
+    def test_instance(self):
+        """Test instantation"""
+        self.assertIsInstance(self.storage, FileStorage)
+
+    def test_all_returns_dict(self):
+        """Test that all() method returns dictionary"""
+        result = self.storage.all()
+        self.assertIsInstance(result, dict)
+
+    def test_new_adds_object_to_objects(self):
+        """Test that new() method adds object to __object dictionary"""
+        obj = BaseModel()
+        self.storage.new(obj)
+        key = "{}.{}".format(obj._class_._name_, obj.id)
+        self.assertIn(key, self.storage.all())
+
+    def test_save_writes_to_file(self):
+        """Test that save() method writes to file"""
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.storage.save
+        with open(FileStorage.FileStorage_file_path, mode="r") as f:
+        data = json.load(f)
+        key = "{}.{}".format(obj._class_._name_, obj.id)
+        self.assertIn(key, data)
+
+    def test_reload_loads_from_file(self):
+        """Test that reload() method loads from file"""
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.storage.save()
+        key = "{}.{}".format(obj._class_._name_, obj.id)
+        self.storage.FileStorage_objects[key].name = "Alice"
+        self.storage.reload()
+        self.assertNotEqual(self.storage.FileStorage_objects[key].name, "Alice")
 
     if __name__ == '__main__':
         unittest.main()
